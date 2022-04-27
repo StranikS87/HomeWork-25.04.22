402cf094fd7306608ada541fefc7912b85d8aae2
«Dmitriy <vorotov87@mail.ru>
2022-04-27 11:07:49 +1000

    Task27

---
+++ HomeWork/Task27/Program.cs
@@ -0,0 +1,13 @@
+﻿//Задача 27: Напишите программу, которая принимает на 
+//вход число и выдаёт сумму цифр в числе.
+
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int result = 0;
+
+for ( int i = 0; i <= number; i++)
+{
+    result = result + i;
+}
+
+Console.WriteLine($"Сумма цифр числа {number} равно: {result}");
