16892483e7492e3f25a7030722026c4f0f6900a5
«Dmitriy <vorotov87@mail.ru>
2022-04-27 10:59:30 +1000

    HW 25.04

---
+++ HomeWork/Task29/Program.cs
@@ -0,0 +1,22 @@
+﻿//Задача 29: Напишите программу, которая задаёт массив 
+//из 8 элементов и выводит их на экран.
+
+void FillArray(int[] array)
+{
+    for( int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 34);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    for (int i =0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+
+int[] mass = new int[8];
+FillArray(mass);
+PrintArray(mass);
